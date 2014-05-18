9e32fe59dc06049f99757950c60c33c250b08f26
tadasalisauskas <tadas@alisauskas.net>
2014-05-18 22:19:11 +0300

    Finished tentative Loader process

--- src/os2/main/processes/imp/Loader.java
+++ src/os2/main/processes/imp/Loader.java
@@ -1,6 +1,7 @@
 package os2.main.processes.imp;
 
 import os2.main.Core;
+import os2.main.hardware.ChannelsDevice.ChannelsDevice;
 import os2.main.processes.Process;
 import os2.main.resources.Resource;
 
@@ -32,16 +33,36 @@ public class Loader extends Process {
 			break;
 		case (1):
 			// Blokuotas, laukiam resurso "Kanalų įrenginys"
+			res = Core.resourceList.searchResource("CHANNELS_DEVICE");
+			if (res != null) {
+				if (res.getParent() == null) {
+					res.setParent(this);
+				} else {
+					this.changeStep(1);
+				}
+			} else {
+				this.changeStep(1);
+			}
 			break;
 		case (2):
 			// Nustatom kanalų įrenginio registrus ir vygdom komandą XCHG
+			ChannelsDevice.ST = 3; // Šaltinis: išorinė atmintis
+			ChannelsDevice.DT = 1; // Tikslas: vartotojo atmintis
+			// ChannelsDevice.DB = vieta vartotojo atmintyje
+			ChannelsDevice.XCHG();
+			this.changeStep(3);
 			break;
 		case (3):
 			// Atlaisvinam "Kanalų įrenginys" resursą
+			res = Core.resourceList.searchResource("CHANNELS_DEVICE");
+			res.removeParent();
+			this.changeStep(4);
 			break;
 		case (4):
 			// Sukuriamas "Iš loader" resursas, skirtas JobGovernor procesui
 			// sukūrusiam gautąjį "Pakrovimo paketo" resursą
+			Core.resourceList.addResource(new Resource("FROM_LOADER"));
+			this.changeStep(0);
 			break;
 		}
 	}
