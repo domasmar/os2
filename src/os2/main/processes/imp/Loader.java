c83a3f650ab3314029b010114c95d44630644559
tadasalisauskas <tadas@alisauskas.net>
2014-05-18 21:30:59 +0300

    Started implementing Loader

--- src/os2/main/processes/imp/Loader.java
+++ src/os2/main/processes/imp/Loader.java
@@ -1,20 +1,34 @@
 package os2.main.processes.imp;
 
+import os2.main.Core;
 import os2.main.processes.Process;
+import os2.main.resources.Resource;
 
 /**
  * Šio proceso paskirtis – išorinėje atmintyje esančius blokus 
  * perkelti į vartotojo atmintį.
- * @author domas
+ * @author Tadas
  *
  */
+
 public class Loader extends Process {
 
 	@Override
 	public void nextStep() {
+		Resource res;
+		String programName;
+		int destinationAddress;
 		switch (this.step) {
 		case (0):
 			// Blokuotas, laukiam resurso "Pakrovimo paketas"
+			res = Core.resourceList.searchResource("LOADER_PACKET");
+			if (res != null) {
+				programName = (String) res.getInformation(); // gausiu iš Artūro programos pavadinimą
+				destinationAddress = (int) res.getInformation(); // iš kur gauti vartotojo atminties adresą, kur rašyti programą?
+				this.changeStep(1);
+			} else {
+				this.changeStep(0);
+			}
 			break;
 		case (1):
 			// Blokuotas, laukiam resurso "Kanalų įrenginys"
