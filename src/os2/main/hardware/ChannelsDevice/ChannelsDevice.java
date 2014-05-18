7767a7b00e7dc40f3b89d0f71d8e55cae50ea727
tadasalisauskas <tadas@alisauskas.net>
2014-05-18 13:32:58 +0300

    Created ChannelsDevice, implemented JobToSwap

---
+++ src/os2/main/hardware/ChannelsDevice/ChannelsDevice.java
@@ -0,0 +1,10 @@
+package os2.main.hardware.ChannelsDevice;
+
+public class ChannelsDevice {
+	
+	public static int SB;
+	public static int DB;
+	public static int ST;
+	public static int DT;
+
+}
--- src/os2/main/processes/imp/JobToSwap.java
+++ src/os2/main/processes/imp/JobToSwap.java
@@ -1,6 +1,8 @@
 package os2.main.processes.imp;
 
+import os2.main.Core;
 import os2.main.processes.Process;
+import os2.main.resources.Resource;
 
 /**
  * Šio proceso paskirtis – perkelti užduoties programos blokus iš supervizorinės atminties į
@@ -12,16 +14,38 @@ public class JobToSwap extends Process {
 
 	@Override
 	public void nextStep() {
+		Resource res;
 		switch (this.step) {
 		case (0):
 			// Blokuotas, laukiam resurso "Užduoties vygdymo parametrai
-			// supervizorinėje atmintyje" resurso
+			// supervizorinėje atmintyje" resurso,
+			res = Core.resourceList.searchResource("EXECUTION_PARAMETERS");
+			if (res != null) {
+				this.changeStep(this.step + 1);
+			}
+			else {
+				this.changeStep(0);
+			}
 			break;
 		case (1):
 			// Blokuotas laukiant "Išorinė atmintis" resurso
+			res = Core.resourceList.searchResource("HDD");
+			if (res != null) {
+				this.changeStep(this.step + 1);
+			}
+			else {
+				this.changeStep(1);
+			}
 			break;
 		case (2):
 			// Blokuotas laukiant "Kanalų įrenginys" resurso
+			res = Core.resourceList.searchResource("CHANNELS_DEVICE");
+			if (res != null) {
+				this.changeStep(this.step + 1);
+			}
+			else {
+				this.changeStep(2);
+			}
 			break;
 		case (3):
 			// Nustatinėjami kanalų įrenginio registra ir vygdoma komanda "XCHG"
