3f7ff0e3471138f00d81ccfdb364d94fd97db3b0
ArthurasJ <jankus.arturas@gmail.com>
2014-05-21 18:53:39 +0300

    Added type of resources final values.

---
+++ src/os2/main/resources/ResourceType.java
@@ -0,0 +1,20 @@
+package os2.main.resources;
+
+/**
+ *
+ * @author Arturas
+ */
+public class ResourceType {
+    public static final String VIRT_MEM = "VIRTUAL_MEMORY";
+    public static final String SUP_MEM = "SUPERVISOR_MEMORY";
+    public static final String EXT_MEM = "EXTERNAL_MEMORY";
+    public static final String LOAD_PACK = "LOADER_PACKET";
+    public static final String PACK_FROM_LOAD = "PACKET_FROM_LOADER";
+    public static final String INT = "INTERRUPT_RESOURCE";
+    public static final String CH_DEV = "CHANNEL_DEVICE";
+    public static final String EXEC_PAR = "EXECUTION_PARAMETERS";
+    public static final String TASK_IN_EXT_MEM = "TASK_IN_EXTERNAL_MEMORY";
+    public static final String LI_IN_MEM = "LINE_IN_MEMORY";
+    public static final String CPU = "CENTRAL_PROCESSING_UNIT";
+    public static final String END = "END_OF_OPERATING_SYSTEM";   
+}
