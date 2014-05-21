5932f2483c0ff1a0913e76c67df8edaa38f04bed
tadasalisauskas <tadas@alisauskas.net>
2014-05-21 17:16:31 +0300

    Initialized PrintLine process

--- src/os2/main/processes/imp/PrintLine.java
+++ src/os2/main/processes/imp/PrintLine.java
@@ -1,6 +1,8 @@
 package os2.main.processes.imp;
 
+import os2.main.Core;
 import os2.main.processes.Process;
+import os2.main.resources.Resource;
 
 /**
  * Šio proceso paskirtis – į išvedimo srautą pasiųsti 
@@ -12,12 +14,33 @@ public class PrintLine extends Process {
 
 	@Override
 	public void nextStep() {
+		Resource res;
 		switch (this.step) {
 		case (0):
-			// Blokuotas, laukiam "Eilutė atmintyje" resurso 
+			// Blokuotas, laukiam "Eilutė atmintyje" resurso
+			res = Core.resourceList.searchResource("LINE_IN_MEMORY");
+			if (res != null) {
+				this.changeStep(1);
+			}
+			else {
+				this.changeStep(0);
+			}
 			break;
 		case (1):
 			// Blokuotas, laukiam "Kanalų įrenginio" resurso
+			res = Core.resourceList.searchResource("CHANNELS_DEVICE");
+			if (res != null) {
+				if (res.getParent() == null || res.getParent() == this) {
+					res.setParent(this);
+					this.changeStep(this.step + 1);
+				}
+				else {
+					this.changeStep(2);
+				}
+			}
+			else {
+				this.changeStep(2);
+			}
 			break;
 		case (2):
 			// Nustatinėjami įrenginio registra ir įvygdoma komanda XCHG
