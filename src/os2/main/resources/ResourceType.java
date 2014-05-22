e023e1d3d72c4b54392cd127f31eec950cf465fe
Domas <domas933@gmail.com>
2014-05-22 21:35:10 +0300

    New resource types added

--- src/os2/main/resources/ResourceType.java
+++ src/os2/main/resources/ResourceType.java
@@ -8,6 +8,7 @@ public class ResourceType {
     public static final String VIRT_MEM = "VIRTUAL_MEMORY";
     public static final String SUP_MEM = "SUPERVISOR_MEMORY";
     public static final String EXT_MEM = "EXTERNAL_MEMORY";
+    public static final String USER_MEM = "USER_MEMORY";
     public static final String FILE_NAME = "FILE_NAME_OF_PROGRAM";
     public static final String LOAD_PACK = "LOADER_PACKET";
     public static final String PACK_FROM_LOAD = "PACKET_FROM_LOADER";
@@ -17,5 +18,7 @@ public class ResourceType {
     public static final String TASK_IN_EXT_MEM = "TASK_IN_EXTERNAL_MEMORY";
     public static final String LI_IN_MEM = "LINE_IN_MEMORY";
     public static final String CPU = "CENTRAL_PROCESSING_UNIT";
-    public static final String END = "END_OF_OPERATING_SYSTEM";   
+    public static final String END = "END_OF_OPERATING_SYSTEM";
+    public static final String PROGRAM_IN_SUPERVISOR = "PROGRAM_IN_SUPERVISOR";
+    public static final String PROGRAM_FROM_FLASH = "PROGRAM_FROM_FLASH";
 }
