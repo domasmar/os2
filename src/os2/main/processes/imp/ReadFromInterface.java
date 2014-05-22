12fb0aaae58b0b4357cd6febf5d9532d7f68caa1
Domas <domas933@gmail.com>
2014-05-22 22:30:25 +0300

    Updated resource names

--- src/os2/main/processes/imp/ReadFromInterface.java
+++ src/os2/main/processes/imp/ReadFromInterface.java
@@ -8,8 +8,10 @@ import os2.main.Core;
 import os2.main.hardware.memory.RMMemory;
 import os2.main.processes.Process;
 import os2.main.resources.Resource;
+import os2.main.resources.ResourceType;
 import os2.main.resources.descriptors.FromGUIDescriptor;
 import os2.main.resources.descriptors.PrintDescriptor;
+import os2.main.resources.descriptors.ProgramInSupervisorDescriptor;
 
 /**
  * Šį procesą kuria ir naikina procesas „StartStop“. Proceso paskirtis – gavus
@@ -23,18 +25,21 @@ public class ReadFromInterface extends Process {
 	private String fileName;
 	private int[] content;
 
+	private int programStart;
+	private int programEnd;
+
 	@Override
 	public void nextStep() {
 		Resource res;
 		switch (this.step) {
 		case (0):
 			// Blokuojam kol sulaukiam resurso "Iš vartojo sąsajos
-			res = Core.resourceList.searchResource("FROM_GUI");
+			res = Core.resourceList.searchResource(ResourceType.PROGRAM_FROM_FLASH);
 			if (res != null) {
 				FromGUIDescriptor descriptor = (FromGUIDescriptor) res
 						.getDescriptor();
 				this.fileName = descriptor.getFileName();
-				Core.resourceList.delete("FROM_GUI");
+				Core.resourceList.delete(ResourceType.PROGRAM_FROM_FLASH);
 				this.changeStep(this.step + 1);
 			} else {
 				this.changeStep(0);
@@ -43,7 +48,8 @@ public class ReadFromInterface extends Process {
 		case (1):
 			// Nuskaitom failą
 			try {
-				byte byteContent[] = Files.readAllBytes(Paths.get(this.fileName));
+				byte byteContent[] = Files.readAllBytes(Paths
+						.get(this.fileName));
 				this.content = new int[byteContent.length];
 				for (int i = 0; i < byteContent.length; i++) {
 					this.content[i] = (new Byte(byteContent[i])).intValue();
@@ -51,7 +57,7 @@ public class ReadFromInterface extends Process {
 				this.changeStep(this.step + 1);
 			} catch (IOException e) {
 				e.printStackTrace();
-				Resource r = new Resource("PRINT");
+				Resource r = new Resource(ResourceType.LI_IN_MEM);
 				PrintDescriptor printDescriptor = new PrintDescriptor();
 				printDescriptor.setMessage("Klaida: Failas: " + this.fileName
 						+ " neegzistuoja arba jo nepavyko atidaryti");
@@ -61,7 +67,7 @@ public class ReadFromInterface extends Process {
 			}
 			break;
 		case (2):
-			res = Core.resourceList.searchResource("SUPERVISOR");
+			res = Core.resourceList.searchResource(ResourceType.SUP_MEM);
 			if (res != null) {
 				if (res.getParent() == null) {
 					res.setParent(this);
@@ -75,18 +81,22 @@ public class ReadFromInterface extends Process {
 			break;
 		case (3):
 			// Kopijuojam blokus į supervizorinę atmintį
-			res = Core.resourceList.searchResource("SUPERVISOR");
+			res = Core.resourceList.searchResource(ResourceType.SUP_MEM);
 			if (res != null) {
 				if (res.getParent() == this) {
 					res.removeParent();
-					if (RMMemory.loadProgramToMemory(this.content)) {
+					this.programStart = RMMemory.loadProgramToMemory(this.content);
+					
+					if (programStart > 0) {
+						this.programEnd = this.programStart + this.content.length + 1;
 						this.changeStep(this.step + 1);
 					} else {
-						Resource r = new Resource("PRINT");
+						Resource r = new Resource(ResourceType.LI_IN_MEM);
 						PrintDescriptor printDescriptor = new PrintDescriptor();
-						printDescriptor.setMessage("Klaida: Failas: "
-								+ this.fileName
-								+ " negali būti perkeltas į supervizorinę atmintį, nes yra per didelis");
+						printDescriptor
+								.setMessage("Klaida: Failas: "
+										+ this.fileName
+										+ " negali būti perkeltas į supervizorinę atmintį, nes yra per didelis");
 						r.setDescriptor(printDescriptor);
 						Core.resourceList.addResource(r);
 						this.changeStep(0);
@@ -101,8 +111,14 @@ public class ReadFromInterface extends Process {
 			break;
 		case (4):
 			// Sukuriamas resursas "Užduotis supervizorinėje atmintyje"
-			Core.resourceList
-					.addResource(new Resource("PROGRAM_IN_SUPERVISOR"));
+			Resource r = new Resource(ResourceType.PROGRAM_IN_SUPERVISOR);
+			ProgramInSupervisorDescriptor descriptor = new ProgramInSupervisorDescriptor();
+			descriptor.setProgramBegin(this.programStart);
+			descriptor.setProgramEnd(this.programEnd);
+			String[] name = this.fileName.split("/");
+			descriptor.setProgramName(name[name.length - 1]);
+			r.setDescriptor(descriptor);
+			Core.resourceList.addResource(r);
 			this.changeStep(0);
 			break;
 		}
