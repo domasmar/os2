9c715af20da285dab7d4e6f7f8708549f12a5c8c
Domas <domas933@gmail.com>
2014-05-22 22:30:44 +0300

    Updated resource names

--- src/os2/main/processes/imp/StartStop.java
+++ src/os2/main/processes/imp/StartStop.java
@@ -5,6 +5,7 @@ import os2.main.processes.Process;
 import os2.main.processes.ProcessStatus;
 import os2.main.processes.ProcessType;
 import os2.main.resources.Resource;
+import os2.main.resources.ResourceType;
 
 /**
  * Šis procesas atsakingas už sistemos darbo pradžią ir pabaigą. Įjungus kompiuterį šis procesas
@@ -22,27 +23,27 @@ public class StartStop extends Process {
 		switch (step) {
 		case 0:
 			// Sisteminiu resursų inicializacija
-			Core.resourceList.addResource(new Resource("HDD"));
-			Core.resourceList.addResource(new Resource("MEMORY"));
-			Core.resourceList.addResource(new Resource("CPU"));
-			Core.resourceList.addResource(new Resource("SUPERVISOR"));
-			Core.resourceList.addResource(new Resource("CHANNELS_DEVICE"));
+			Core.resourceList.addResource(new Resource(ResourceType.EXT_MEM));
+			Core.resourceList.addResource(new Resource(ResourceType.USER_MEM));
+			Core.resourceList.addResource(new Resource(ResourceType.CPU));
+			Core.resourceList.addResource(new Resource(ResourceType.SUP_MEM));
+			Core.resourceList.addResource(new Resource(ResourceType.CH_DEV));
 			this.changeStep(this.step + 1);
 			break;
 		case 1:
 			// Sisteminių permanentinių procesų inicializacija
 			Core.processQueue.add(new ReadFromInterface());
-//			Core.processQueue.add(new JCL());
-//			Core.processQueue.add(new JobToSwap());
-//			Core.processQueue.add(new Loader());
+			Core.processQueue.add(new JCL());
+			Core.processQueue.add(new JobToSwap());
+			Core.processQueue.add(new Loader());
 //			Core.processQueue.add(new MainProc());
 //			Core.processQueue.add(new Interrupt());
-//			Core.processQueue.add(new PrintLine());
+			Core.processQueue.add(new PrintLine());
 			this.changeStep(this.step + 1);
 			break;
 		case 2:
 			// Blokavimas laukiant "OS pabaiga" resurso
-			if (Core.resourceList.searchResource("END_OF_OS") != null) {
+			if (Core.resourceList.searchResource(ResourceType.END) != null) {
 				this.changeStep(this.step + 1);
 			} else {
 				this.changeStep(2);
