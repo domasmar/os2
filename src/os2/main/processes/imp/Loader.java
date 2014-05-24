83b77a5ef4e4bf4f6d8d1e3bb5e96435864cb27a
tadasalisauskas <tadas@alisauskas.net>
2014-05-24 21:50:52 +0300

    Fixed Loader

--- src/os2/main/processes/imp/Loader.java
+++ src/os2/main/processes/imp/Loader.java
@@ -5,6 +5,7 @@ import os2.main.hardware.ChannelsDevice.ChannelsDevice;
 import os2.main.hardware.memory.VMMemory;
 import os2.main.processes.Process;
 import os2.main.resources.Resource;
+import os2.main.resources.ResourceType;
 import os2.main.resources.descriptors.FromLoaderDescriptor;
 import os2.main.resources.descriptors.LoaderPacketDescriptor;
 
@@ -27,9 +28,10 @@ public class Loader extends Process {
 		switch (this.step) {
 		case (0):
 			// Blokuotas, laukiam resurso "Pakrovimo paketas"
-			res = Core.resourceList.searchResource("LOADER_PACKET");
+			res = Core.resourceList.searchResource(ResourceType.LOAD_PACK);
 			if (res != null) {
 				LoaderPacketDescriptor descriptor = (LoaderPacketDescriptor) res.getDescriptor();
+				res.setParent(this);
 				this.programName = descriptor.getFilename();
 				this.vmm = descriptor.getMemory();
 				this.jg = (JobGovernor) res.getParent();
@@ -40,7 +42,7 @@ public class Loader extends Process {
 			break;
 		case (1):
 			// Blokuotas, laukiam resurso "Kanalų įrenginys"
-			res = Core.resourceList.searchResource("CHANNELS_DEVICE");
+			res = Core.resourceList.searchResource(ResourceType.CH_DEV);
 			if (res != null) {
 				if (res.getParent() == null) {
 					res.setParent(this);
@@ -63,7 +65,7 @@ public class Loader extends Process {
 			break;
 		case (3):
 			// Atlaisvinam "Kanalų įrenginys" resursą
-			res = Core.resourceList.searchResource("CHANNELS_DEVICE");
+			res = Core.resourceList.searchResource(ResourceType.CH_DEV);
 			res.removeParent();
 			this.changeStep(4);
 			break;
@@ -76,6 +78,7 @@ public class Loader extends Process {
 			descriptor.setMessage("Programa " + this.programName + " iš išorinės atminties perkelta į vartotojo");
 			res.setDescriptor(descriptor);
 			Core.resourceList.addResource(new Resource("FROM_LOADER"));
+			Core.resourceList.deleteChildResource(this, ResourceType.LOAD_PACK);
 			this.changeStep(0);
 			break;
 		}
