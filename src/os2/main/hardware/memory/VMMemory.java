07b5a476149e5e6a96f35e5aa00f099e314abb8a
Domas <domas933@gmail.com>
2014-05-24 16:18:46 +0300

    Functions to load and save CPU state

--- src/os2/main/hardware/memory/VMMemory.java
+++ src/os2/main/hardware/memory/VMMemory.java
@@ -4,7 +4,7 @@ import os2.main.hardware.CPU;
 
 public class VMMemory {
 	private static final int VIRTUAL_MEMORY_SIZE = 256;
-	
+
 	private int ptr = 0;
 	private int pageNumber = 0;
 
@@ -25,7 +25,7 @@ public class VMMemory {
 		if (address / 16 <= this.ptr / (16 * 16)) {
 			RMMemory.set(getRealAddress(address), value);
 		} else {
-			CPU.setPI((byte)1); 
+			CPU.setPI((byte) 1);
 			throw new RuntimeException("PI = 1");
 		}
 	}
@@ -39,6 +39,71 @@ public class VMMemory {
 		}
 	}
 	
+	public void loadCPUState() {
+		int i = 0;
+		CPU.setAX(this.get(i++));
+		CPU.setBX(this.get(i++));
+		CPU.setPTR(this.get(i++));
+		CPU.setC((byte) this.get(i++));
+		CPU.setCHST0((byte) this.get(i++));
+		CPU.setCHST1((byte)this.get(i++));
+		CPU.setCHST2((byte)this.get(i++));
+		CPU.setCS((short) this.get(i++));
+		CPU.setDS((short)this.get(i++));
+		CPU.setEND((byte)this.get(i++));
+		CPU.setIOI((byte)this.get(i++));
+		CPU.setIP((byte)this.get(i++));
+		CPU.setMODE((byte)this.get(i++));
+		CPU.setPI((byte)this.get(i++));
+		CPU.setSI((byte)this.get(i++));
+		CPU.setSP((byte)this.get(i++));
+		CPU.setSS((byte)this.get(i++));
+		CPU.setSTI((byte)this.get(i++));
+		CPU.setTI((byte)this.get(i++));
+	}
+	
+	public void saveCPUState() {
+		int i = 0;
+		this.set(i++, CPU.getAX());
+		this.set(i++, CPU.getBX());
+		this.set(i++, CPU.getPTR());
+		this.set(i++, CPU.getC());
+		this.set(i++, CPU.getCHST0());
+		this.set(i++, CPU.getCHST1());
+		this.set(i++, CPU.getCHST2());
+		this.set(i++, CPU.getCS());
+		this.set(i++, CPU.getDS());
+		this.set(i++, CPU.getEND());
+		this.set(i++, CPU.getIOI());
+		this.set(i++, CPU.getIP());
+		this.set(i++, CPU.getMODE());
+		this.set(i++, CPU.getPI());
+		this.set(i++, CPU.getSI());
+		this.set(i++, CPU.getSP());
+		this.set(i++, CPU.getSS());
+		this.set(i++, CPU.getSTI());
+		this.set(i++, CPU.getTI());
+	}
+
+	public void loadProgram(int[] program) {
+		// Pirmi 16 int'u registrai
+		// DS, SS, CS
+		int varsSeparatorIndex = 0;
+		for (int i = 0; i < program.length; i++) {
+			if (program[i] == RMMemory.VARS_SEPARATOR) {
+				varsSeparatorIndex = i;
+			}
+		}
+		int varsSize = varsSeparatorIndex;
+		int programSize = program.length - varsSeparatorIndex - 1;
+
+		int blocksForVars = varsSize / RMMemory.BLOCK_SIZE
+				+ (varsSize % RMMemory.BLOCK_SIZE > 0 ? 1 : 0);
+		int blocksForProgram = programSize / RMMemory.BLOCK_SIZE
+				+ (programSize % RMMemory.BLOCK_SIZE > 0 ? 1 : 0);
+
+	}
+
 	public void destroy() {
 		for (int i = 0; i < VIRTUAL_MEMORY_SIZE; i++) {
 			this.set(i, 0);
